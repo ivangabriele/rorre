985d94dfa25747e4e4d0d3a8388de28725f915ae
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-08 22:26:34 +0100

    rorre.d: fix typings & add Rorre#error

--- rorre.d.ts
+++ rorre.d.ts
@@ -1,14 +1,19 @@
+type ReadOnly<T> = {
+  readonly [P in keyof T]: T[P];
+}
+type Stringify<T> = Extract<T, string>
+
 export interface Dictionary {
-  [name: string]: string
+  [name: string]: string;
 }
 
-export interface RorreError extends Error {
-  index: number
-  name: string
-  message: string
+export interface RorreError<T extends Dictionary, P extends string> extends Error {
+  index: number;
+  name: P;
+  message: T[P];
 }
 
-declare interface Rorre {
+interface Rorre<T extends Dictionary = {}> {
   /**
    * Get the complete error dictionary indexed object.
    *
@@ -16,7 +21,7 @@ declare interface Rorre {
    * This is an enumed mapping: for each error, both its #message
    * and generated #index exist as a key, and a value as well.
    */
-  dictionary: Dictionary
+  dictionary: ReadOnly<T>;
 
   /**
    * Get an enum of the dictionary errors' name.
@@ -25,7 +30,20 @@ declare interface Rorre {
    * This is a reverse mapping: for each error, both its #name
    * and generated #index exist as a key, and a value as well.
    */
-  name: string | number
+  error: ReadOnly<{
+    readonly [P in keyof T]: () => ReadOnly<RorreError<T, Stringify<P>>>;
+  }>;
+
+  /**
+   * Get an enum of the dictionary errors' name.
+   *
+   * @description
+   * This is a reverse mapping: for each error, both its #name
+   * and generated #index exist as a key, and a value as well.
+   */
+  name: ReadOnly<{
+    readonly [P in keyof T]: number;
+  }>;
 
   /**
    * Declare the complete errors dictionary.
@@ -33,12 +51,13 @@ declare interface Rorre {
    * @description
    * This method can and must only be called once.
    */
-  declare(dictionary: Dictionary): void
+  declare<T extends Dictionary>(dictionary: T): Rorre<T>;
 
   /**
    * Instanciate (but do NOT throw) a RorreError and return it.
    */
-  emit(name: number | string): RorreError
+  emit<T extends Dictionary>(name: keyof T | number): RorreError<T, Stringify<keyof T>>;
 }
 
-export default Rorre
+declare const rorre: ReadOnly<Rorre>;
+export default rorre;
