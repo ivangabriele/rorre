b5ce0d14fe012e9a031985b7d22fef2b334a7055
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-17 16:49:41 +0100

    rorre.d: fix declare() dictionary param type inference

--- rorre.d.ts
+++ rorre.d.ts
@@ -1,19 +1,16 @@
-export type ReadOnly<T> = {
-  readonly [P in keyof T]: T[P];
-}
+export type ReadOnly<T> = { readonly [P in keyof T]: T[P]; }
 export type Stringify<T> = Extract<T, string>;
 
 export interface Dictionary {
-  [name: string]: string;
+  [key: string]: string;
 }
 
-export interface RorreError<T extends Dictionary, P extends string> extends Error {
-  index: number;
-  name: P;
-  message: T[P];
+export interface RorreError<N extends string, M extends string> extends Error {
+  name: N;
+  message: M;
 }
 
-export interface Rorre<T extends Dictionary = {}> {
+export interface Rorre<D extends Dictionary> {
   /**
    * Get the complete error dictionary indexed object.
    *
@@ -21,14 +18,14 @@ export interface Rorre<T extends Dictionary = {}> {
    * This is an enumed mapping: for each error, both its #message
    * and generated #index exist as a key, and a value as well.
    */
-  dictionary: ReadOnly<T>;
+  readonly dictionary: ReadOnly<D>;
 
   /**
    * Instanciate (but do NOT throw) a RorreError and return it.
    */
-  error: ReadOnly<{
-    readonly [P in keyof T]: RorreError<T, Stringify<P>>;
-  }>;
+  readonly error: {
+    readonly [N in keyof D]: RorreError<Stringify<N>, D[N]>;
+  };
 
   /**
    * Get an enum of the dictionary errors' name.
@@ -37,18 +34,19 @@ export interface Rorre<T extends Dictionary = {}> {
    * This is a reverse mapping: for each error, both its #name
    * and generated #index exist as a key, and a value as well.
    */
-  name: ReadOnly<{
-    readonly [P in keyof T]: number;
-  }>;
+  readonly name: {
+    readonly [N in keyof D]: N;
+  };
+}
 
+declare const rorre: {
   /**
    * Declare the complete errors dictionary.
    *
    * @description
    * This method can and must only be called once.
    */
-  declare(dictionary: Dictionary): Rorre<typeof dictionary>;
-}
+  declare<D extends Dictionary>(dictionary: D): Rorre<D>;
+};
 
-declare const rorre: ReadOnly<Rorre>;
 export default rorre;
